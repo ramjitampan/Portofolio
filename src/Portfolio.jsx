c7e8c4622539f19cf670737d8cf5a620ea1106bd
ramjitampan <ramjijunfaris28@gmail.com>
2025-01-03 20:29:33 +0700

    Update project with new changes

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -47,7 +47,7 @@ function App() {
         <header className="w3-container w3-padding-32 w3-center w3-black" id="home" data-aos="fade-up">
           <h1 className="w3-jumbo"><span className="w3-hide-small">Namaku</span> Ramzy Junfaris</h1>
           <p>Desain grafis and Web Designer.</p>
-          <img src="public/Ramzyganteng.png" alt="boy" className="w3-image" style={{ width: '100%', height: 'auto' }} />
+          <img src="/Ramzyganteng.png" alt="boy" className="w3-image" style={{ width: '100%', height: 'auto' }} />
         </header>
 
         <div className="w3-content w3-justify w3-text-grey w3-padding-64" id="about" data-aos="fade-up">
@@ -77,49 +77,49 @@ function App() {
 
         <div className="gallery-container" data-aos="fade-up">
           <div className="gallery">
-            <img src="public/SKILL ISU.png" alt="Design 1" className="gallery-item" data-aos="zoom-in" />
-            <img src="public/revamp fami x ramzy.png" alt="Design 2" className="gallery-item" data-aos="zoom-in" />
-            <img src="public/Brian Makmur.PNG" alt="Design 3" className="gallery-item" data-aos="zoom-in" />
-            <img src="public/SUNSET.png" alt="Design 4" className="gallery-item" data-aos="zoom-in" />
+            <img src="/SKILL ISU.png" alt="Design 1" className="gallery-item" data-aos="zoom-in" />
+            <img src="/revamp fami x ramzy.png" alt="Design 2" className="gallery-item" data-aos="zoom-in" />
+            <img src="/Brian Makmur.PNG" alt="Design 3" className="gallery-item" data-aos="zoom-in" />
+            <img src="/SUNSET.png" alt="Design 4" className="gallery-item" data-aos="zoom-in" />
             <img src="/SUNSET_UNP.png" alt="Design Square 1" className="gallery-item" data-aos="zoom-in" />
-            <img src="public/fahmix andri.png" alt="Design Square 2" className="gallery-item" data-aos="zoom-in" />
+            <img src="/fahmix andri.png" alt="Design Square 2" className="gallery-item" data-aos="zoom-in" />
           </div>
         </div>
 
         <h3 className="w3-padding-24 w3-text-light-grey" data-aos="fade-left">CEES AWAAK</h3>
         <div className="profiles" data-aos="fade-right">
           <div className="profile" data-aos="zoom-in">
-            <img src="public/Brian.png" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
+            <img src="/Brian.png" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
             <p><span className="w3-large w3-margin-right">Brian Makmur.</span> THE KING OF MAGE</p>
             <p>HERO MASTERY XAVIER</p>
           </div>
           <div className="profile" data-aos="zoom-in">
-            <img src="public/apip.PNG" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
+            <img src="/apip.PNG" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
             <p><span className="w3-large w3-margin-right">APIP</span> THE HIGH MACRO MICRO.</p>
             <p>HERO MASTERY ALUCARD & SUN.</p>
           </div>
           <div className="profile" data-aos="zoom-in">
-            <img src="public/RESTU2.png" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
+            <img src="/RESTU2.png" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
             <p><span className="w3-large w3-margin-right">RESTUTU</span> MANY YOU HAVE THE GREAT JUNGLER EXPE?</p>
             <p>HERO MASTERY BANE</p>
           </div>
           <div className="profile" data-aos="zoom-in">
-            <img src="public/agil.jpg" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
+            <img src="/agil.jpg" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
             <p><span className="w3-large w3-margin-right">ZUPERGILZ</span> THE KING OF THE KING.</p>
             <p>HERO MASTERY LING</p>
           </div>
           <div className="profile" data-aos="zoom-in">
-            <img src="public/FAHMI.PNG" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
+            <img src="/FAHMI.PNG" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
             <p><span className="w3-large w3-margin-right">FAHMI</span> THE GREAT MATILDA.</p>
             <p>HERO MASTERY MATHILDA</p>
           </div>
           <div className="profile" data-aos="zoom-in">
-            <img src="public/ilham.PNG" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
+            <img src="/ilham.PNG" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
             <p><span className="w3-large w3-margin-right">ILHAM</span> THE GOAT OF FHANY.</p>
             <p>HERO MASTERY FHANY</p>
           </div>
           <div className="profile" data-aos="zoom-in">
-            <img src="public/andri.jpg" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
+            <img src="/andri.jpg" alt="Avatar" className="w3-left w3-margin-right" style={{ width: '80px' }} />
             <p><span className="w3-large w3-margin-right">Andri</span> THE KING OF MARKSMAN.</p>
             <p>HERO MASTERY BEATRIX</p>
           </div>
